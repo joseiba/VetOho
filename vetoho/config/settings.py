f12fef55b5b0f1165e2b7ea1acebc7eb87ea33c3
Javier <jparedes@roshka.com>
2022-08-27 13:05:14 -0400

    fix settings en el array INSTALLED_APPS

--- vetoho/config/settings.py
+++ vetoho/config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.user',
-    'apps.cliente'
+    'apps.cliente',
     'apps.inventario.productos',
     'apps.inventario.depositos',
 ]
