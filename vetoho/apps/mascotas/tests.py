7c655ac8fd1bf7452a7d23eeea5129eb7510ccaa
Jose Ibañez <familiajoselo@gmail.com>
2023-04-29 21:52:51 -0400

    se agrega cambios

--- vetoho/apps/mascotas/tests.py
+++ vetoho/apps/mascotas/tests.py
@@ -93,59 +93,7 @@ class MAscotasFormTest(LiveServerTestCase):
     #     driver.quit
 
 
-    # def testMascotaForm(self):
-    #     driver = webdriver.Chrome()
-    #     driver.get(URL_BASE_TEST_FUNTIONAL + 'accounts/login/')
-
-    #     user_name = driver.find_element('name', 'username')
-    #     user_password = driver.find_element('name','password')
-
-    #     submit = driver.find_element('id','button_submit')
-
-    #     user_name.send_keys('joseiba')
-    #     time.sleep(2)
-    #     user_password.send_keys('joseiba')
-    #     time.sleep(2)
-
-    #     submit.send_keys(Keys.RETURN)
-    #     time.sleep(2)	       
-
-    #     driver.get(URL_BASE_TEST_FUNTIONAL + '/mascota/add/')
-    #     time.sleep(2)
-
-    #     nombre = driver.find_element('name', 'nombre_mascota')
-    #     nombre.send_keys('Keisy')
-    #     time.sleep(1)
-
-        
-    #     peso = driver.find_element('name', 'peso')
-    #     peso.send_keys('500')
-    #     time.sleep(1)                
-        
-    #     element = driver.find_element(By.XPATH, "//select[@name='id_raza']")
-    #     all_options = element.find_elements(By.TAG_NAME, "option")
-    #     for option in all_options:
-    #         option.click()
-    #     time.sleep(1)    
-
-    #     element = driver.find_element(By.XPATH, "//select[@name='id_cliente']")
-    #     all_options = element.find_elements(By.TAG_NAME, "option")
-    #     for option in all_options:
-    #         option.click()
-    #     time.sleep(1)    
-
-    #     submit = driver.find_element(By.CSS_SELECTOR,"button[class='btn btn-primary']")
-    #     submit.send_keys(Keys.RETURN)
-    #     time.sleep(3)
-
-    #     submit = driver.find_element(By.CSS_SELECTOR,"button[class='swal2-confirm swal2-styled swal2-default-outline']")
-    #     submit.send_keys(Keys.RETURN)
-    #     time.sleep(3)
-
-    #     driver.quit
-
-
-    def testEditForm(self):
+    def testMascotaForm(self):
         driver = webdriver.Chrome()
         driver.get(URL_BASE_TEST_FUNTIONAL + 'accounts/login/')
 
@@ -160,22 +108,31 @@ class MAscotasFormTest(LiveServerTestCase):
         time.sleep(2)
 
         submit.send_keys(Keys.RETURN)
-        time.sleep(2)
+        time.sleep(2)	       
 
-        driver.get(URL_BASE_TEST_FUNTIONAL + 'mascota/edit/1/')
+        driver.get(URL_BASE_TEST_FUNTIONAL + '/mascota/add/')
         time.sleep(2)
-
-        # submit = driver.find_element(By.CSS_SELECTOR,"button[class='btn btn-outline-primary ml-3 mr-3 mb-3']")
-        # submit.send_keys(Keys.RETURN)
-        # time.sleep(3)
 
         nombre = driver.find_element('name', 'nombre_mascota')
-        nombre.clear()
-        time.sleep(1)       
+        nombre.send_keys('Keisy')
+        time.sleep(1)
 
-        nombre_edit = driver.find_element('name', 'nombre_mascota')
-        nombre_edit.send_keys('keisy editado')
-        time.sleep(1)       
+        
+        peso = driver.find_element('name', 'peso')
+        peso.send_keys('500')
+        time.sleep(1)                
+        
+        element = driver.find_element(By.XPATH, "//select[@name='id_raza']")
+        all_options = element.find_elements(By.TAG_NAME, "option")
+        for option in all_options:
+            option.click()
+        time.sleep(1)    
+
+        element = driver.find_element(By.XPATH, "//select[@name='id_cliente']")
+        all_options = element.find_elements(By.TAG_NAME, "option")
+        for option in all_options:
+            option.click()
+        time.sleep(1)    
 
         submit = driver.find_element(By.CSS_SELECTOR,"button[class='btn btn-primary']")
         submit.send_keys(Keys.RETURN)
@@ -186,5 +143,48 @@ class MAscotasFormTest(LiveServerTestCase):
         time.sleep(3)
 
         driver.quit
+
+
+    # def testEditForm(self):
+    #     driver = webdriver.Chrome()
+    #     driver.get(URL_BASE_TEST_FUNTIONAL + 'accounts/login/')
+
+    #     user_name = driver.find_element('name', 'username')
+    #     user_password = driver.find_element('name','password')
+
+    #     submit = driver.find_element('id','button_submit')
+
+    #     user_name.send_keys('joseiba')
+    #     time.sleep(2)
+    #     user_password.send_keys('joseiba')
+    #     time.sleep(2)
+
+    #     submit.send_keys(Keys.RETURN)
+    #     time.sleep(2)
+
+    #     driver.get(URL_BASE_TEST_FUNTIONAL + 'mascota/edit/1/')
+    #     time.sleep(2)
+
+    #     # submit = driver.find_element(By.CSS_SELECTOR,"button[class='btn btn-outline-primary ml-3 mr-3 mb-3']")
+    #     # submit.send_keys(Keys.RETURN)
+    #     # time.sleep(3)
+
+    #     nombre = driver.find_element('name', 'nombre_mascota')
+    #     nombre.clear()
+    #     time.sleep(1)       
+
+    #     nombre_edit = driver.find_element('name', 'nombre_mascota')
+    #     nombre_edit.send_keys('keisy editado')
+    #     time.sleep(1)       
+
+    #     submit = driver.find_element(By.CSS_SELECTOR,"button[class='btn btn-primary']")
+    #     submit.send_keys(Keys.RETURN)
+    #     time.sleep(3)
+
+    #     submit = driver.find_element(By.CSS_SELECTOR,"button[class='swal2-confirm swal2-styled swal2-default-outline']")
+    #     submit.send_keys(Keys.RETURN)
+    #     time.sleep(3)
+
+    #     driver.quit
 
 
